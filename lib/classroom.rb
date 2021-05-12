47846d293438895b33205c5983c5f91c88c938f1
Miriam Aceves <miriammerari02@gmail.com>
2021-05-12 17:59:14 -0400

    Initial Commit

--- lib/classroom.rb
+++ lib/classroom.rb
@@ -1,2 +1,18 @@
 class Classroom
+  attr_reader :subject, :capacity, :students
+
+  def initialize(subject, capacity)
+    @subject = subject
+    @capacity = capacity
+    @students = []
+  end
+
+  def add_student(student)
+    @students << student
+  end
+
+  def yell_at_students()
+    @students.map!(&:upcase)
+  end
+
 end
